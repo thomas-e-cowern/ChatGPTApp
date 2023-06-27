ca754c9815aab3e3147df0f188def091800f1707
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-27 14:41:48 -0400

    update main view and chatgpt app

--- Shared/MainView.swift
+++ Shared/MainView.swift
@@ -6,13 +6,24 @@
 //
 
 import SwiftUI
+import OpenAISwift
 
 struct MainView: View {
     
     @State private var chatText: String = ""
+    @State private var answers: [String] = []
+    
+    let openIAKey = Bundle.main.infoDictionary?["CHAT_GPT_KEY"]
+    
+    private var isFormValid: Bool {
+        chatText.isEmptOrWhiteSpaces
+    }
     
     var body: some View {
         VStack {
+            List(answers, id: \.self) { answer in
+                Text(answer)
+            }
             Spacer()
             HStack {
                 TextField("Search...", text: $chatText)
@@ -20,16 +31,33 @@ struct MainView: View {
                     .padding(.leading, 10)
                 
                 Button {
-                    // more to come
+                    performSearch()
                 } label: {
                     Image(systemName: "paperplane.circle.fill")
                         .font(.title)
                         .rotationEffect(Angle(degrees: 45.0))
                 }
                 .padding(.trailing, 10)
+                .disabled(isFormValid)
             }
   
 
+        }
+    }
+    
+    private func performSearch() {
+        
+        let openAI = OpenAISwift(authToken: openIAKey as! String)
+        
+        openAI.sendCompletion(with: chatText, maxTokens: 500) { result in
+            switch result {
+            case .success(let success):
+                print(success)
+                let answer = success.choices?.first?.text.trimmingCharacters(in: .whitespacesAndNewlines) ?? ""
+                answers.append(answer)
+            case .failure(let failure):
+                print(failure)
+            }
         }
     }
 }
