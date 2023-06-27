17fd32ddc337319318c488e39fb2df84ddcab191
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-27 15:25:08 -0400

    add managers folder and core data mabager file

---
+++ Shared/Managers/CoreDataManager.swift
@@ -0,0 +1,26 @@
+//
+//  CoreDataManager.swift
+//  iOSChatGPT
+//
+//  Created by Thomas Cowern on 6/27/23.
+//
+
+import Foundation
+import CoreData
+
+class CoreDataManager {
+    
+    let persistentContainer: NSPersistentContainer
+    
+    static let shared = CoreDataManager()
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "HistoryModel")
+        persistentContainer.loadPersistentStores { description, error in
+            if let error = error {
+                fatalError("Core Data Store failed: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+}
