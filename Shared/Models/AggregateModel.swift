751424c766abd265edb4c850299ab2044e498657
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-28 08:46:13 -0400

    add aggregate model

---
+++ Shared/Models/AggregateModel.swift
@@ -0,0 +1,24 @@
+//
+//  AggregateModel.swift
+//  ChatGPTApp
+//
+//  Created by Thomas Cowern on 6/28/23.
+//
+
+import Foundation
+
+class AggregateModel: ObservableObject {
+    
+    @Published var queries: [QueryModel] = []
+    
+    func saveQuery(_ query: QueryModel) throws {
+        
+        let viewContext = CoreDataManager.shared.persistentContainer.viewContext
+        let historyItem = HistoryItem(context: viewContext)
+        historyItem.question = query.question
+        historyItem.answer = query.answer
+        historyItem.dateCreated = Date()
+        try viewContext.save()
+    }
+    
+}
