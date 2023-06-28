abc79d2205872e72cf661ac311eb2bf3e870356b
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-28 11:56:52 -0400

    update main view to add progress view to main view

--- Shared/Views/MainView.swift
+++ Shared/Views/MainView.swift
@@ -13,6 +13,7 @@ struct MainView: View {
     @EnvironmentObject private var model: AggregateModel
     
     @State private var chatText: String = ""
+    @State private var isSearching: Bool = false
     
     let openIAKey = Bundle.main.infoDictionary?["CHAT_GPT_KEY"]
     
@@ -54,12 +55,14 @@ struct MainView: View {
                     .padding(.leading, 10)
                 
                 Button {
+                    isSearching = true
                     performSearch()
                 } label: {
                     Image(systemName: "paperplane.circle.fill")
                         .font(.title)
                         .rotationEffect(Angle(degrees: 45.0))
                 }
+                .buttonStyle(.borderless)
                 .padding(.trailing, 10)
                 .disabled(isFormValid)
             }
@@ -69,6 +72,11 @@ struct MainView: View {
         .padding(10)
         .onChange(of: model.query) { query in
             model.queries.append(query)
+        }
+        .overlay(alignment: .center) {
+            if isSearching {
+                ProgressView("Searching...")
+            }
         }
     }
     
@@ -92,7 +100,11 @@ struct MainView: View {
                     print(error.localizedDescription)
                 }
                 
+                chatText = ""
+                isSearching = false
+                
             case .failure(let failure):
+                isSearching = false
                 print(failure)
             }
         }
