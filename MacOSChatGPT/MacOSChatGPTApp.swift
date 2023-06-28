1bd9c29f9908babaacf2029f942a2c1fcd4a01c8
Thomas Cowern <thomas.e.cowern@gmail.com>
2023-06-28 11:32:25 -0400

    add appdelegate to macos app

--- MacOSChatGPT/MacOSChatGPTApp.swift
+++ MacOSChatGPT/MacOSChatGPTApp.swift
@@ -9,6 +9,9 @@ import SwiftUI
 
 @main
 struct MacOSChatGPTApp: App {
+    
+    @NSApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
@@ -16,4 +19,39 @@ struct MacOSChatGPTApp: App {
                 .environmentObject(AggregateModel())
         }
     }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    private var statusItem: NSStatusItem!
+    private var popover: NSPopover!
+    
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        
+        let contentView = ContentView().environment(\.managedObjectContext, CoreDataManager.shared.persistentContainer.viewContext)
+            .environmentObject(AggregateModel())
+        
+        statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+        
+        if let statusButton = statusItem.button {
+            statusButton.image = NSImage(systemSymbolName: "brain", accessibilityDescription: "Brain")
+            statusButton.action = #selector(togglePopover)
+        }
+        
+        self.popover = NSPopover()
+        self.popover.contentSize = NSSize(width: 800, height: 600)
+        self.popover.behavior = .transient
+        self.popover.contentViewController = NSHostingController(rootView: contentView)
+    }
+    
+    @objc func togglePopover() {
+        if let button = statusItem.button {
+            if popover.isShown {
+                self.popover.performClose(nil)
+            } else {
+                self.popover.show(relativeTo: button.bounds, of: button, preferredEdge: .minY)
+            }
+        }
+    }
+    
 }
